65bc6b523164a97facf35a349874e2f2b21f4e7f
Sasha Voynow <sasha@labzero.com>
2016-10-15 15:38:20 -0700

    a simple form

--- src/static/index.js
+++ src/static/index.js
@@ -6,3 +6,5 @@ require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' ); 
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/Main' );
 Elm.Main.embed( document.getElementById( 'main' ) );
+
+
