ce8bfb15bea044544a7f0a5a94fe55773bc2c4bb
Sasha Voynow <sasha@labzero.com>
2016-10-16 13:45:10 -0700

    Cmd for getting geocoding data

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -4,6 +4,9 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.App as App
+import Http
+import Geocoding as G
+import Task exposing (Task)
 
 
 -- app
@@ -25,12 +28,16 @@ main =
 
 type alias Model =
     { locationField : String
+    , geocodingData : Maybe G.Response
+    , errorMessage : Maybe String
     }
 
 
 initialModel : Model
 initialModel =
     { locationField = ""
+    , geocodingData = Nothing
+    , errorMessage = Nothing
     }
 
 
@@ -41,6 +48,8 @@ initialModel =
 type Msg
     = UpdateLocationField String
     | SubmitLocation
+    | GeocodingError Http.Error
+    | GeocodingSuccess G.Response
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -50,7 +59,13 @@ update msg model =
             { model | locationField = str } ! []
 
         SubmitLocation ->
-            { model | locationField = "" } ! []
+            { model | locationField = "" } ! [ geocodeLocation model.locationField ]
+
+        GeocodingError err ->
+            { model | errorMessage = Just <| toString err } ! []
+
+        GeocodingSuccess data ->
+            { model | geocodingData = Just data } ! []
 
 
 
@@ -59,12 +74,40 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "body" ]
-        [ Html.form [ class "pure-form " ]
+    div [ class "body pure-g" ]
+        [ Html.form [ class "pure-form pure-u-2-3" ]
             [ fieldset []
                 [ legend [] [ text "Find a Bike Share" ]
                 , input [ class "pure-input-1-3", type' "text", placeholder "Address, Location Name or Postal Code", value model.locationField, onInput UpdateLocationField ] []
                 , button [ class "pure-button pure-button-primary", type' "button", onClick SubmitLocation ] [ text "Search" ]
                 ]
             ]
+        , errorDiv model.errorMessage
+        , div [] [ model.geocodingData |> Maybe.map toString |> Maybe.withDefault "" |> text ]
         ]
+
+
+errorDiv : Maybe String -> Html Msg
+errorDiv errorMsg =
+    case errorMsg of
+        Just str ->
+            div [ class "error pure-u-2-3" ] [ text str ]
+
+        _ ->
+            div [ class "noerror " ] []
+
+
+
+-- Cmds
+
+
+googleKey : String
+googleKey =
+    "AIzaSyAji7Gm0r66d9QfW1aPYGSocFlawSXNLMw"
+
+
+geocodeLocation : String -> Cmd Msg
+geocodeLocation str =
+    G.requestForAddress googleKey str
+        |> G.send
+        |> Task.perform GeocodingError GeocodingSuccess
