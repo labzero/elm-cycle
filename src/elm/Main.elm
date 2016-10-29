92e558be7566e687e13cfbcb3c581752c59edb9b
Sasha Voynow <sasha@labzero.com>
2016-10-29 13:01:09 -0700

    Squashed commit of the following:
    commit 8c818bf23d093d015e3e32239dac494c44a423fb
    Author: Sasha Voynow <sasha@labzero.com>
    Date:   Mon Oct 17 13:23:41 2016 -0700
    
        Squashed commit of the following:
    
        commit 563030c39feeddd0b37a6e6046e273cd56b8cb0e
        Author: Sasha Voynow <sasha@labzero.com>
        Date:   Fri Oct 14 09:46:54 2016 -0700
    
            setup boilerplate
    
    commit 65bc6b523164a97facf35a349874e2f2b21f4e7f
    Author: Sasha Voynow <sasha@labzero.com>
    Date:   Sat Oct 15 15:38:20 2016 -0700
    
        a simple form

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,35 +1,70 @@
+module Main exposing (..)
+
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Html.App as App
 
+
 -- app
+
+
 main : Program Never
-main = App.program
-  {
-    init = {} ! []
-  , update = update
-  , view = view
-  , subscriptions = \_ -> Sub.none
-  }
+main =
+    App.program
+        { init = initialModel ! []
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
 
 
 -- MODEL
-type alias Model = {}
+
+
+type alias Model =
+    { locationField : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { locationField = ""
+    }
+
 
 
 -- UPDATE
-type Msg = NoOp 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+type Msg
+    = UpdateLocationField String
+    | SubmitLocation
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp -> model ! []
+    case msg of
+        UpdateLocationField str ->
+            { model | locationField = str } ! []
+
+        SubmitLocation ->
+            { model | locationField = "" } ! []
 
 
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
-view : Model -> Html Msg
-view model = div [] [text "success!"]
- 
 
+
+view : Model -> Html Msg
+view model =
+    div [ class "body" ]
+        [ Html.form [ class "pure-form " ]
+            [ fieldset []
+                [ legend [] [ text "Find a Bike Share" ]
+                , input [ class "pure-input-1-3", type' "text", placeholder "Address, Location Name or Postal Code", value model.locationField, onInput UpdateLocationField ] []
+                , button [ class "pure-button pure-button-primary", type' "button", onClick SubmitLocation ] [ text "Search" ]
+                ]
+            ]
+        ]
